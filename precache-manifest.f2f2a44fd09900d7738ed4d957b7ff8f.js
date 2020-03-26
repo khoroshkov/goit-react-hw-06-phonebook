0ed79b1aeae54531817b8a25dc5f7009a76aa709
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-03-26 12:07:28 +0200

    Updates

---
+++ precache-manifest.f2f2a44fd09900d7738ed4d957b7ff8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd8c7e4621122224f77e7b614f71d6b",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "edc071e4b2271c6d54ba",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.5f61f152.chunk.css"
+  },
+  {
+    "revision": "c6189d9c23395e67bc8a",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.678a77dc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.678a77dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc071e4b2271c6d54ba",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.b9fa9e22.chunk.js"
+  },
+  {
+    "revision": "891bc6c146fbebc80bb8",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.fea6bfc1.js"
+  }
+]);
